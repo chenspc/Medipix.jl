34de29026349d237e16a26cbd4d2c611a09e98ea
Documenter.jl <documenter@juliadocs.github.io>
2022-06-04 12:40:43 +0000

    build based on b189faf

--- v0.3.0/search_index.js
+++ v0.3.0/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MedipixMerlinEM","category":"page"},{"location":"#MedipixMerlinEM","page":"Home","title":"MedipixMerlinEM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MedipixMerlinEM]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Medipix","category":"page"},{"location":"#Medipix","page":"Home","title":"Medipix","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Medipix]","category":"page"}]
 }
