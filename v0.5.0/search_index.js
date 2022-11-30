a9fd47f9731b9a91c09c02634a807a5a17b5980e
Documenter.jl <documenter@juliadocs.github.io>
2022-11-30 13:12:06 +0000

    build based on d8da633

---
+++ v0.5.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Medipix","category":"page"},{"location":"#Medipix","page":"Home","title":"Medipix","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Medipix]","category":"page"}]
+}
